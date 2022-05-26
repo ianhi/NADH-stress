ceb6702f6e42c6c4e19d75980e733b2685ddbc2d
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2022-05-26 16:06:11 -0400

    updates

--- generate_events.py
+++ generate_events.py
@@ -7,7 +7,7 @@ def update_event_exp(
     mda: MDASequence,
     expByPosition: Dict[str, List[float]],
 ):
-    n_pos = mda.shape[mda.axis_order.index('p')]
+    n_pos = mda.shape[mda.axis_order.index("p")]
     events = []
     for e in mda.iter_events():
         # e.exposure = 400
@@ -24,7 +24,7 @@ def update_event_exp(
         # events.append(e)
     # for e in events:
     #     pass
-        # print(e)
+    # print(e)
     return events
 
 
@@ -46,6 +46,45 @@ class fakeSequence:
 
     def __iter__(self) -> Iterator[MDAEvent]:  # type: ignore
         yield from self.iter_events()
+
+    def iter_chunked(self, over: str) -> Iterator[List[MDAEvent]]:
+        """
+        Generate events in chunks where only the given *over* index changes
+
+        Parameters
+        ----------
+        seq : MDASequence
+        over : str
+            The index keys over which to allow grouping. E.g. 'z' to return
+            all events in the z stack for a given time point, position and channel.
+
+        yields
+        ------
+        chunk : list of events
+        """
+
+        over = over.lower()
+
+        def extract_index(event):
+            return tuple(
+                event.index.get(key) for key in event.index.keys() if key not in over
+            )
+
+        events = self.iter_events()
+        e = next(events)
+        cur_chunk = extract_index(e)
+
+        chunk = [e]
+        for e in events:
+            if extract_index(e) == cur_chunk:
+                chunk.append(e)
+            else:
+                yield chunk
+                chunk = [e]
+                cur_chunk = extract_index(e)
+        if len(chunk) > 0:
+            # ensure we always return all events
+            yield chunk
 
 
 def genSequence() -> fakeSequence:
